32873c231ebf208a21974e604f3a5af245da2d72
Herediano <juanh5@live.com.mx>
2019-10-25 17:17:12 -0500

    Updates

---
+++ precache-manifest.637d7473ef8c2dc456c383970257f114.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4d960506ad0213cb693c032b80729e",
+    "url": "/innovestdemo/index.html"
+  },
+  {
+    "revision": "c3082ea45e078bbf8d74",
+    "url": "/innovestdemo/static/css/main.714ce98a.chunk.css"
+  },
+  {
+    "revision": "6101f90209acedc86c98",
+    "url": "/innovestdemo/static/js/2.9ca7dd54.chunk.js"
+  },
+  {
+    "revision": "c3082ea45e078bbf8d74",
+    "url": "/innovestdemo/static/js/main.49dd0254.chunk.js"
+  },
+  {
+    "revision": "c7a81557210409a8e19f",
+    "url": "/innovestdemo/static/js/runtime-main.08ca28b1.js"
+  },
+  {
+    "revision": "b90ad17f3c6e10af51bd8e9d4f7e14fe",
+    "url": "/innovestdemo/static/media/BackgroundImage-Innovest.b90ad17f.png"
+  },
+  {
+    "revision": "fb1d0ba14fde8b8ffd6fcf728f02707d",
+    "url": "/innovestdemo/static/media/logo-Innovest.fb1d0ba1.png"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/innovestdemo/static/media/stars.1d17360a.svg"
+  }
+]);
